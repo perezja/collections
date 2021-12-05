91c8b29505dc5ca7fc9905679e88c3fa1f1eab88
jperez9315 <jperez9315@gmail.com>
2021-12-04 18:35:32 -0800

    added `Groups` feature to `Bucket`

--- routines/bucket.py
+++ routines/bucket.py
@@ -1,8 +1,11 @@
 from object import Object
-from typing import TypeVar, Optional, Mapping, List
+from typing import TypeVar, Optional, Iterable, Mapping, List
 from common import Record
+import re
+import json
 
 from collections import defaultdict
+from collections import namedtuple
 
 T = TypeVar("T")
 
@@ -11,7 +14,7 @@ class Index:
 
     slots = ()
 
-    def __get__(self, obj, cls=None):
+    def __get__(self, obj: Iterable[Object], cls=None):
         if obj is None:
             return self
 
@@ -20,27 +23,62 @@ class Index:
             index[item.id].append(item.name)  
 
         return index
+    
+class Groups():
+    slots = ()
+
+    Field = namedtuple('Field', 'name pattern regex')
+
+    def __set__(self, obj, value: Mapping[str, str]):
+        setattr(obj, '_groups', list())
+
+        for field_name, pattern in value.items():
+            _field = self.Field(field_name, pattern, re.compile(pattern))
+            obj._groups.append(_field)
+
+    def __get__(self, obj, cls=None):
+        if obj is None or not hasattr(obj, '_groups'):
+            return self
+        
+        #groups = defaultdict(list)
+        groups = defaultdict(dict)
+
+        for grp in obj._groups:
+            for idx, dlist in obj.index.items():
+                matches = list(map(lambda x: grp.regex.match(x), dlist))
+                if not any(matches):
+                    #groups[idx].append((grp.name, None))
+                    groups[idx][grp.name] = None
+                    continue
+
+                if sum(isinstance(x, re.Match) for x in matches) > 1:
+                    raise RuntimeError("Scalar field '{}' matched multiple objects for index '{}'".format(grp.name, idx))
+
+                value = list(filter(bool, matches)).pop().string
+                #groups[idx].append({grp.name, value))
+                groups[idx][grp.name] = value
+
+        return groups 
 
 class Bucket:
     """An object bucket"""
-    slots = ("_data")
+    slots = ("_data", "_groups")
 
     _data: List[Object] = list()
     _Object = Object
 
     index = Index()
+    groups = Groups()
     
-    def __init__(self, pattern: str, *, flt: Optional[Mapping[str, T]]=None): 
+    def __init__(self, pattern: str, *, groups: Optional[Mapping[str, str]], flt: Optional[Mapping[str, T]]=None): 
         self._Object.set_parameters(pattern=pattern, flt=flt)
+        self.groups = groups
 
     def __call__(self, data: Record):
         self._store(data)
 
-    def __iter__(self):
-        return iter(self.data)
-
     def __len__(self):
-        return(len(self._items))
+        return(len(self.index))
 
     def _store(self, data: Record):
         if data is None:
@@ -54,32 +92,18 @@ class Bucket:
     def __iter__(self):
         return(iter(self._data)) 
 
-"""
-class Patterns:
-    slots = ('_patterns') 
+    def __repr__(self):
+        return "{}(len={}, items={})".format(
+            self.__class__.__name__,
+            len(self),
+            str(self)
+            )
 
-    def __init__(self):
-        self._patterns = {
-                'index': None,
-                'groups': {}
-        }
+    def __str__(self):
+        sample = list(self.index.keys())[:3]
 
-    @property
-    def index:
-        return self._patterns['index']
+        d = self.groups if hasattr(self, '_groups') else self.index 
+        d = { k:v for k,v in d.items() if k in sample }
 
-    @index.setter
-    def index(self, value)
-        self._patterns['index'] = value
-
-    @property
-    def groups:
-        return self._patterns['groups']
-
-    @groups.setter
-    def groups(self, mapping: Mapping[str, str])
-    
-        group_name = mapping.key
-        self._patterns['groups'][group_name] = mapping.value 
-"""
+        return json.dumps(d, indent=2)[:-1] + "  ...\n}"
 
--- routines/group.py
+++
@@ -1,9 +0,0 @@
-class Group:
-    """A group of related data"""
-
-    slots = ("_id", "_data")
-
-    name  = NamePart() 
-    id    = IDPart()
-    index = Index()
-
